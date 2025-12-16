308cfa0cd8c40691ad8d6eac956c1dee61ccf816
Shamuoon Haider <shamuoonh08@gmail.com>
2025-12-16 16:06:00 +0500

    nested dynamic routes

---
+++ routing/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,16 @@
+interface ReviewPageProps {
+  params: Promise<{ productId: string; reviewId: string }>;
+}
+
+const Page = async ({ params }: ReviewPageProps) => {
+  const { productId, reviewId } = await params;
+  return (
+    <div>
+      <h1>
+        Product Detiails for {productId} and Review {reviewId}
+      </h1>
+    </div>
+  );
+};
+
+export default Page;
