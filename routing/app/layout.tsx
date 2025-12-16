46693a059b0e875a3c2d682e2e8a5eacd4a0bdaa
Shamuoon Haider <shamuoonh08@gmail.com>
2025-12-16 15:39:59 +0500

    created simple routes

---
+++ routing/app/layout.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Routing",
+  description: "Routing in Next js",
+};
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
