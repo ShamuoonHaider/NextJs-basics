0fbc9a7b97af001432b8c53778b8d1bc001e326a
Shamuoon Haider <shamuoonh08@gmail.com>
2025-12-17 15:08:52 +0500

    Error recovery

--- routing/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ routing/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,7 +1,30 @@
 "use client";
 
-const ErrorBoundary = ({ error }: { error: Error }) => {
-  return <div>{error.message}</div>;
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+const ErrorBoundary = ({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) => {
+  const router = useRouter();
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+  return (
+    <div>
+      <p>{error.message}</p>
+      <button onClick={() => reload()} className="bg-amber-700 cursor-pointer">
+        Try again
+      </button>
+    </div>
+  );
 };
 
 export default ErrorBoundary;
