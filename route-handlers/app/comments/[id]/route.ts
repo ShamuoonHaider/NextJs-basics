e101bae6631375e15a6db4ee098dc3a23683f1a0
Shamuoon Haider <shamuoonh08@gmail.com>
2025-12-19 13:34:55 +0500

    PATCH http request

--- route-handlers/app/comments/[id]/route.ts
+++ route-handlers/app/comments/[id]/route.ts
@@ -4,8 +4,19 @@ interface Props {
   params: Promise<{ id: string }>;
 }
 
-export const GET = async (request: Request, { params }: Props) => {
+export const GET = async (_request: Request, { params }: Props) => {
   const { id } = await params;
   const comment = comments.find((c) => c.id === parseInt(id));
   return Response.json(comment);
+};
+
+export const PATCH = async (request: Request, { params }: Props) => {
+  const { id } = await params;
+  const body = await request.json();
+  const { text } = body;
+
+  const index = comments.findIndex((c) => c.id === parseInt(id));
+  comments[index].text = text;
+
+  return Response.json(comments[index]);
 };
