55fac9acc67218cd2cbae72a1768c6b255e25c45
Shamuoon Haider <shamuoonh08@gmail.com>
2025-12-24 12:33:17 +0500

    client-only

--- rendering/app/client-route/page.tsx
+++ rendering/app/client-route/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { useTheme } from "@/components/theme-provider";
+import { clientOnlyFunction } from "@/utils/client-utils";
 
 const ClientRoutePage = () => {
   const theme = useTheme();
+  const result = clientOnlyFunction();
   return (
     <div>
       <h2 style={{ color: theme.colors.secondary }}>Client router page</h2>
+      <p>{result}</p>
     </div>
   );
 };
---
+++ rendering/utils/client-utils.ts
@@ -0,0 +1,8 @@
+import "client-only";
+
+export const clientOnlyFunction = () => {
+  console.log(
+    "use window object use local storage or any client side only code"
+  );
+  return "client only function";
+};
